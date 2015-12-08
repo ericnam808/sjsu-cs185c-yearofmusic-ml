7f35d9cb20267e491cbd339b8a58d1520a798c7f
hexgrinder <anon@anon.com>
2015-12-08 12:14:39 -0800

    Integration of MultinomialNB. Removed references of prev classifiers.

--- nb_classifier.py
+++ nb_classifier.py
@@ -7,16 +7,14 @@ Naive bayesian classifier using sklearn.
 """
 
 from numpy import *
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import SGDClassifier
-#from sklearn.svm import LinearSVC
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
 from sklearn import metrics
 
 import csv
 
-def splice_csv_columns(data_filepath, columns):
+def splice_delimited_columns(data_filepath, columns):
 
     features = []
     with open(data_filepath, 'rb') as csvfile:
@@ -30,8 +28,10 @@ def splice_csv_columns(data_filepath, columns):
 def splice_columns(arr, indexes):
     if len(indexes) == 1:
         return map(lambda ar: ar[indexes[0]].lower(), arr)
-    return map(lambda ar: reduce(lambda x,y: x + ' ' + y, map(lambda i: ar[i].lower(), indexes)), arr)
-
+    return map(
+        lambda ar: reduce(
+            lambda x,y: x + ' - ' + y,
+            map(lambda i: ar[i].lower(), indexes)), arr)
 
 def build_unique_list(iter_):
     return list(set(iter_))
@@ -40,48 +40,56 @@ def index_list(iter_, codes):
     return map(lambda v: codes.index(v), iter_)
 
 def train_classifer(classifer, training_file, test_file, columns, class_col, data_cols):
-    
-    raw = splice_csv_columns(
+
+    raw = splice_delimited_columns(
         training_file, columns)
 
     raw_classes = splice_columns(raw,[class_col])
-    training_features = splice_columns(raw, [1,2])
+    training_features = splice_columns(raw, data_cols)
+
+    print raw_classes[:3]
+    print training_features[:3]
     
-    print raw_classes[:10]
-    print training_features[:10]
-    
-    count_vect = CountVectorizer()
+    count_vect = CountVectorizer(
+        decode_error='replace',
+        strip_accents='unicode',
+        binary=False)
+
+    X_train_counts = count_vect.fit_transform(training_features)
+
     # Vectorize by tf-idf
     tfidf_transformer = TfidfTransformer()
-    X_train_counts = count_vect.fit_transform(training_features)
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
     
     classifer.fit(X_train_tfidf.toarray(), raw_classes)
 
-    test_raw = splice_csv_columns(
+    ### Validation Section ###
+    
+    test_raw = splice_delimited_columns(
         test_file, columns)
 
     test_classes = splice_columns(test_raw,[class_col])
-    test_features = splice_columns(test_raw, [1,2])
+    test_features = splice_columns(test_raw, data_cols)
 
     X_new_counts = count_vect.transform(test_features)
     X_new_tfidf = tfidf_transformer.transform(X_new_counts)
     
     class_names = build_unique_list(raw_classes)
-    #class_vect = index_list(raw_classes, class_names)
+
+    class_vect = index_list(raw_classes, class_names)
 
     results = classifer.predict(X_new_tfidf.toarray())
     
-    report = metrics.classification_report(test_classes, results, labels=None, target_names=class_names)
+    report = metrics.classification_report(
+        test_classes, results, labels=None)
     
-    print report
     return report
 
 def main():
 
     columns = [0,2,3]
     class_col = 0
-    feature_cols = [2,3]
+    feature_cols = [1,2]
 
     def execute_test(title, clf, output_filepath):
 
@@ -90,8 +98,8 @@ def main():
         print 'Training...'
         results = train_classifer(
             clf,
-            r'data/training_tracks_MIN5.csv',
-            r'data/test_tracks_MIN5.csv',
+            r'data/debug_combo2.txt',
+            r'data/debug_combo2_test.txt',
             columns,
             class_col,
             feature_cols)
@@ -99,13 +107,14 @@ def main():
         print 'Prediction...'
 
         print 'Results...'
+        print results 
+
         with open(output_filepath, 'w') as outfile: 
             outfile.write(results)
                 
     classifers = [
-        ('GaussianNB - Gaussian Naive Bayes',
-#             SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42),
-                GaussianNB(),
+        ('MultinomialNB - Artist_Title Year Classifier',
+                MultinomialNB(alpha=.01),
                 r'results/nb_result_2.txt') ]
     
     # execute test
